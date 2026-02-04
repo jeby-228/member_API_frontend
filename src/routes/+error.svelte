b6350f4ff1f86f542f892c0640eaa9be5e13ad05
jeby-228 <aa4188075@gmail.com>
2026-02-04 11:13:03 +0800

    fixL fmt

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="container">
+	<div class="error-content">
+		<h1 class="--badge-warning">{$page.status} {$page.error?.message || '發生錯誤'}</h1>
+	</div>
+</div>
