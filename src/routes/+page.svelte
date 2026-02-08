39f5384d2dd77b8264ebdf69f6f2b149e6b020d1
jeby-0228 <aa4188075@gmail.com>
2026-02-08 15:59:28 +0800

    fix: 修正步驟顯示元件的索引使用方式以提高穩定性

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,13 @@
 	const steps = [
 		{ title: 'First', content: 'First do this.' },
 		{ title: 'Then', content: 'Then do that.' },
-		{ title: 'Finally', content: 'Almost done...' },
+		{ title: 'Finally', content: 'Almost done...' }
 	];
 </script>
 
 <Steps count={steps.length} class="w-full">
 	<Steps.List>
-		{#each steps as item, index}
+		{#each steps as item, index (index)}
 			<Steps.Item {index}>
 				<Steps.Trigger>
 					<Steps.Indicator>{index + 1}</Steps.Indicator>
@@ -22,13 +22,13 @@
 			</Steps.Item>
 		{/each}
 	</Steps.List>
-	{#each steps as item, index}
+	{#each steps as item, index (index)}
 		<Steps.Content {index}>
 			{item.content}
 		</Steps.Content>
 	{/each}
 	<Steps.Content index={steps.length}>All done!</Steps.Content>
-	<div class="flex justify-between items-center gap-2">
+	<div class="flex items-center justify-between gap-2">
 		<Steps.PrevTrigger class="btn preset-filled">Back</Steps.PrevTrigger>
 		<Steps.NextTrigger class="btn preset-filled">Next</Steps.NextTrigger>
 	</div>
