4443b984176abb980116e7d4cbcf0cd98ca88a47
JEBY <76581458+jeby-228@users.noreply.github.com>
2026-02-16 00:09:36 +0800

    fix favicon path (#36)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,6 @@
 	});
 	// google analytics 4
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import AppHeader from '$lib/components/AppHeader.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
 
@@ -21,15 +20,10 @@
 	let searchQuery = $state('');
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
 <AppHeader bind:showSearch bind:searchQuery onMenuToggle={() => (showSidebar = !showSidebar)} />
 
 <div class="relative flex h-[calc(100vh-4rem)]">
 	<Sidebar bind:show={showSidebar} />
-
 	<!-- Main Content -->
 	<main class="flex-1 overflow-y-auto p-4">
 		{@render children()}
